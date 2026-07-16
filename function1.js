1c7fbbd48e48129ba392038808d9431fc4807295
VedisVigourous <vadanta592007@hotmail.com>
2026-07-16 16:01:13 +0530

    Function Added: Star with Greet

---
+++ function1.js
@@ -0,0 +1,15 @@
+function lineOfStars(){
+    let star = ""
+    for (let i = 0; i < 5; i++) {
+        star += "*"
+    }
+    console.log(star)
+}
+
+console.log()
+lineOfStars()
+console.log("Hello")
+lineOfStars()
+console.log("World")
+lineOfStars()
+console.log()
